716793276aaaf5dda89cd477b7e3b2546a5cc052
Randall Wolfe <randywolfe12@gmail.com>
2023-11-28 20:52:41 -0600

    Finished Hello World assignment

--- Python/Python_Fundamentals/Python_Fundamentals/Wolfe_Randall_Hello_World/hello_world.py
+++ Python/Python_Fundamentals/Python_Fundamentals/Wolfe_Randall_Hello_World/hello_world.py
@@ -1,16 +1,16 @@
-# 1. TASK: print "Hello World"
-print( your code here )
-# 2. print "Hello Noelle!" with the name in a variable
+
+print("Hello World")
+
 name = "Noelle"
-print( your code here )	# with a comma
-print( your code here )	# with a +
-# 3. print "Hello 42!" with the number in a variable
+print( "Hello ",name )	
+print("Hello " + name)	
+
 name = 42
-print( your code here )	# with a comma
-print( your code here )	# with a +	-- this one should give us an error!
-# 4. print "I love to eat sushi and pizza." with the foods in variables
+print("Hello ", name)
+print(f"Hello {name}" )
+print("Hello " + name)	
+
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print( your code here ) # with .format()
-print( your code here ) # with an f string
-
+print(f"I love to eat {fave_food1} and {fave_food2}" ) 
+print( "I love to eat {} and {}." .format(fave_food1, fave_food2)) 
