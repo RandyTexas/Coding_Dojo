10a06c66f793fafac1eca4c140adc8d7d92a41e9
Randall Wolfe <randywolfe12@gmail.com>
2023-11-28 20:55:12 -0600

    Hello Wolrd

--- Python/Python_Fundamentals/Python_Fundamentals/Wolfe_Randall_Hello_World/hello_world.py
+++ Python/Python_Fundamentals/Python_Fundamentals/Wolfe_Randall_Hello_World/hello_world.py
@@ -1,16 +1,15 @@
-
 print("Hello World")
 
 name = "Noelle"
-print( "Hello ",name )	
-print("Hello " + name)	
+print( "Hello ",name )
+print("Hello " + name)
 
 name = 42
 print("Hello ", name)
-print(f"Hello {name}" )
-print("Hello " + name)	
+print(f"Hello {name}")
+print("Hello " + name)
 
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print(f"I love to eat {fave_food1} and {fave_food2}" ) 
-print( "I love to eat {} and {}." .format(fave_food1, fave_food2)) 
+print(f"I love to eat {fave_food1} and {fave_food2}" )
+print( "I love to eat {} and {}." .format(fave_food1, fave_food2))
