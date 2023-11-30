6626f3356b4c068a26ce6c125ce50d3d11c36f64
Randall Wolfe <randywolfe12@gmail.com>
2023-11-29 22:51:56 -0600

    for loop basic1

---
+++ Python/Python_Fundamentals/Python_Fundamentals/hello_world_test.py
@@ -0,0 +1,25 @@
+"""
+This is a simple Python script that demonstrates basic functionality.
+
+It prints a welcome message, runs a loop, and selects a random day of the week.
+"""
+
+import random
+
+print('Welcome to Python!')
+
+print('This is a loop printing 5 times')
+for x in range(1, 6):
+    print(f'x is: {x}')
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+day = random.choice(weekdays)
+
+print(f'Today is: {day}')
+
+if day == 'Monday':
+    print('It will be a long week!')
+elif day == 'Friday':
+    print('Woohoo, time for the weekend!')
+else:
+    print('Not quite there yet.')
