33da8a0dcab93a91a30ea3a49161db7159bf3149
Randall Wolfe <randywolfe12@gmail.com>
2024-01-21 13:27:27 -0600

    Dojo Pets

---
+++ Python/Python_Fundamentals/Python_Fundamentals/Wolfe_Randall_Functions_Basic_II/Functions_Basic_II.py
@@ -0,0 +1,28 @@
+
+# # 1 Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
+# i = int(input("Pick a Number"))
+# for num in range(i,-1,-1):
+#     print(num)
+
+# # 2 Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
+# def print_return(a):
+#     print(a[0])
+#     return a[1]
+# results =print_return([1,2])
+# print(results)
+
+# # 3 First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
+# x = [1,2,3,4,5,6,7,8,9]
+# def sum(x):
+#     y = x[0] + len(x)
+#     return y
+# answer = sum(x)
+# print(answer)
+
+# 4 Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
+# 
+#
+#
+
+
+# 5 This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
