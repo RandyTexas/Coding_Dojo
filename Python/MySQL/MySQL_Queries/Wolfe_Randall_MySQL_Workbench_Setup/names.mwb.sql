815277d58fc616596b661e9b5e953fb14122f22a
Randall Wolfe <randywolfe12@gmail.com>
2024-01-30 20:34:08 -0600

    Fixed snake case

---
+++ Python/MySQL/MySQL_Queries/Wolfe_Randall_MySQL_Workbench_Setup/names.mwb.sql
@@ -0,0 +1,13 @@
+SELECT * FROM names
+
+-- INSERT INTO names(name) VALUES("Randall");
+SET SQL_SAFE_UPDATES = 0;
+
+
+-- UPDATE names
+-- SET name = "Terry"
+
+-- UPDATE names
+-- SET name = "Randall"
+-- WHERE id =1;
+
