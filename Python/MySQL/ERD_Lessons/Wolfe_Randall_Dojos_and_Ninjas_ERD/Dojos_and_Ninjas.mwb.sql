4f60985abe88fb8586355574307dc8d3c7acb068
Randall Wolfe <randywolfe12@gmail.com>
2024-01-31 20:18:48 -0600

    Starting Flask

--- Python/MySQL/ERD_Lessons/Wolfe_Randall_Dojos_and_Ninjas_ERD/Dojos_and_Ninjas.mwb.sql
+++ Python/MySQL/ERD_Lessons/Wolfe_Randall_Dojos_and_Ninjas_ERD/Dojos_and_Ninjas.mwb.sql
@@ -1,4 +1,3 @@
-
         
 CREATE TABLE dojos
 (
@@ -6,16 +5,6 @@ CREATE TABLE dojos
   created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   name       VARCHAR(225) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE dojos_ninjas
-(
-  id         INT       NOT NULL AUTO_INCREMENT,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  dojo_id    INT       NOT NULL,
-  ninja_id   INT       NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -27,15 +16,14 @@ CREATE TABLE ninjas
   first_name VARCHAR(225) NOT NULL,
   last_name  VARCHAR(225) NOT NULL,
   age        INT          NOT NULL,
+  dojo_id    INT          NOT NULL,
   PRIMARY KEY (id)
 );
 
-ALTER TABLE dojos_ninjas
-  ADD CONSTRAINT FK_dojos_TO_dojos_ninjas
+ALTER TABLE ninjas
+  ADD CONSTRAINT FK_dojos_TO_ninjas
     FOREIGN KEY (dojo_id)
     REFERENCES dojos (id);
 
-ALTER TABLE dojos_ninjas
-  ADD CONSTRAINT FK_ninjas_TO_dojos_ninjas
-    FOREIGN KEY (ninja_id)
-    REFERENCES ninjas (id);
+        
+      
